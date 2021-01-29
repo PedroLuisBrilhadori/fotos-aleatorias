5844f89afe804ff3a940f9e91273a70962510416
PedroLuisBrilhadori <74775672+PedroLuisBrilhadori@users.noreply.github.com>
2021-01-28 22:11:15 -0300

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+var images = document.createElement('img');
+var fotos = [];
+
+
+
+
+function random(max, min){
+    let valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+
+
+function Refresh(){
+    location.reload();
+}
